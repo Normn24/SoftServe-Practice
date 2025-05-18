fb83f0cb9b1283f8f5c8050b83366c968482a67b
Artur Kudyrko <kudyrkoartur@gmail.com>
2025-05-18 13:57:57 +0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Card/SessionCard.tsx 	modified:   src/components/Movies/Movies.tsx 	modified:   src/components/Sessions/Sessions.tsx

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -1,32 +1,31 @@
 import axios from 'axios';
-import {  useEffect, useState } from 'react'
+import {  useEffect, useState} from 'react'
 import Card from '../Card/Card';
 import Spinner from '../Spinner/Spinner';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllMovies } from '../../store/allMovies';
+import { RootState, AppDispatch } from '../../store/store';
+import { StatusEnum } from '../../utils/EnumsFile';
 
 export default function Movies() {
-  const [movies, setMovies] = useState<any[]>([])
-  const [loading, setLoading] = useState<boolean>()
+  const movies = useSelector((state: RootState) => state.allMovies.allMovie)
+  const status = useSelector((state: RootState) => state.allMovies.status)
+  const dispatch = useDispatch<AppDispatch>()
 
   useEffect(() => {
-    setLoading(true);
-    const apiUrl = 'https://soft-serve-practice-back.vercel.app/api/movies-in-cinema';
-    axios.get(apiUrl)
-    .then((res) => {
-      console.log(res.data);
-      setMovies(res.data);
-      setLoading(false);
-    })
+    dispatch(fetchAllMovies())
+
   }, [])
 
 
   return (
-    <div className='p-9'>
+    <div className='p-9 pt-30'>
       <div className='flex justify-between'>
-        <h1 className='text-3xl font-bold'>Movies List</h1>
+        <h1 className='text-3xl font-bold text-black dark:text-white'>Movies List</h1>
       </div>
         
 
-      {loading === true ? (
+      {status === StatusEnum.LOADING ? (
         <Spinner/>      
       ) : (
         <div className='flex flex-wrap gap-5 justify-start mt-5'>
